3cd89778582d6e9b6c42a24b0ab6ffad68ce0da0
zak10110 <zr380678644997@gmail.com>
2024-06-11 12:55:56 +0300

    Add Kilometers to Miles Converter

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,14 +2,20 @@ package app;
 
 public class Main {
     public static  final double MILES_TO_KILOMETERS_COEFFICIENT=1.60934;
+    public static final double KILOMETERS_TO_MILES_COEFFICIENT = 0.621371;
     public static void main(String[] args){
         System.out.println("Distance Units Converter app.");
         double mls=5.0;
+        double klm=10.0;
         double kilometers=convMilesToKilometers(mls);
-        System.out.println("Result is "+kilometers+" kilometers");
+        double miles=convKilometersToMiles(klm);
+        System.out.println("Result is "+kilometers+" kilometers and "+miles+" miles");
     }
 
     public static double convMilesToKilometers(double miles){
         return miles*MILES_TO_KILOMETERS_COEFFICIENT;
+    }
+    public static double convKilometersToMiles(double kilometers){
+        return kilometers*KILOMETERS_TO_MILES_COEFFICIENT;
     }
 }
